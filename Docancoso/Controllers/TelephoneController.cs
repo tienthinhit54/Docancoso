923b83f92e7001289de60d69fe570e12f37f89b4
tienthinhit54 <dtthinhit.54@gmail.com>
2023-12-12 16:17:14 +0700

    Add project files.

---
+++ Docancoso/Controllers/TelephoneController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Docancoso.Models;
+
+namespace Docancoso.Controllers
+{
+    public class TelephoneController : Controller
+    {
+        // GET: Telephone
+        dbTelephoneDataDataContext data = new dbTelephoneDataDataContext();
+        private List<SANPHAM> Layspmoi(int count)
+        {
+            //Sắp xếp sách theo ngày cập nhật, sau đó lấy top @count 
+            return data.SANPHAMs.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+        public ActionResult Index()
+        {
+            var spmoi = Layspmoi(5);
+            return View(spmoi);
+        }
+        public ActionResult Details(int id)
+        {
+            var sp = from s in data.SANPHAMs
+                       where s.Masp == id
+                       select s;
+            return View(sp.Single());
+        }
+    }
+}
---
+++ Docancoso/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Docancoso.MvcApplication" Language="C#" %>
